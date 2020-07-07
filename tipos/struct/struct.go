a7aa79e8e519673bfa77b644c26d76691d722bcf
Joao Ramos <49027278+jpvramos@users.noreply.github.com>
2020-07-07 00:07:24 -0300

    Struct tipo
    Segue o Struct Tipo

--- tipos/struct/struct.go
+++ tipos/struct/struct.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type produto struct {
 	nome     string
 	preco    float64
@@ -7,10 +9,21 @@ type produto struct {
 }
 
 // Método: função com receiver (receptor)
-func (p_produto) precoComDesconto() float64 {
+func (p produto) precoComDesconto() float64 {
 	return p.preco * (1 - p.desconto)
 }
 
 func main() {
+	var produto1 produto
+	produto1 = produto{
+
+		nome:     "Lapis",
+		preco:    1.79,
+		desconto: 0.05,
+	}
+	fmt.Println(produto1, produto1.precoComDesconto())
+
+	produto2 := produto{"Notebook", 1989.90, 0.10}
+	fmt.Println(produto2, produto2.precoComDesconto())
 
 }
